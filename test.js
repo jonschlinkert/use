8659d18dfe1f1eef0343a581b04345e5dc88518e
tunnckoCore <mameto_100@mail.bg>
2016-05-09 07:14:47 +0300

    add tests for extending app options with plugin options

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ require('mocha');
 var assert = require('assert');
 var define = require('define-property');
 var use = require('./');
+var extend = require('extend-shallow');
 
 describe('use', function() {
   it('should export a function', function() {
@@ -11,7 +12,7 @@ describe('use', function() {
   });
 
   it('should throw TypeError `app` not a function or object', function() {
-    function fixture () {
+    function fixture() {
       use(123);
     }
     assert.throws(fixture, TypeError);
@@ -19,11 +20,57 @@ describe('use', function() {
   });
 
   it('should throw TypeError if not a function passed to `.use` method', function() {
-    function fixture () {
+    function fixture() {
       use({}).use(123);
     }
     assert.throws(fixture, TypeError);
     assert.throws(fixture, /expect `fn` be function/);
+  });
+
+  it('should allow passing `opts.fn` to merge options from each plugin to app options', function() {
+    var limon = {options: {
+      foo: 'bar'
+    }};
+    use(limon, {
+      fn: function(app, options) {
+        assert.strictEqual(this.options.foo, 'bar');
+        this.options = extend(this.options, options);
+        this.options.qux = 123;
+      }
+    });
+
+    limon
+      .use(function() {
+        assert.strictEqual(this.options.foo, 'bar');
+        assert.strictEqual(this.options.xxx, 'yyy');
+        assert.strictEqual(this.options.qux, 123);
+      }, { xxx: 'yyy' })
+      .use(function() {
+        assert.strictEqual(this.options.foo, 'bar');
+        assert.strictEqual(this.options.xxx, 'yyy');
+        assert.strictEqual(this.options.qux, 123);
+        assert.strictEqual(this.options.ccc, 'ddd');
+      }, { ccc: 'ddd' });
+  });
+
+  it('should not extend options if `opts.fn` not given (#3)', function () {
+    var limon = {options: {
+      foo: 'bar'
+    }};
+    use(limon);
+
+    limon
+      .use(function() {
+        assert.strictEqual(this.options.foo, 'bar');
+        assert.strictEqual(this.options.xxx, undefined);
+        assert.strictEqual(this.options.qux, undefined);
+      }, { xxx: 'yyy' })
+      .use(function() {
+        assert.strictEqual(this.options.foo, 'bar');
+        assert.strictEqual(this.options.xxx, undefined);
+        assert.strictEqual(this.options.qux, undefined);
+        assert.strictEqual(this.options.ccc, undefined);
+      }, { ccc: 'ddd' });
   });
 
   it('should decorate "use" onto the given object', function() {
